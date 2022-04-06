9aa9c008fd371e859f15fbd2b6c4b606c1ec7ce2
Ernie Francis <Devin_98@msn.com>
2022-04-06 19:30:38 -0400

    the godfather part 3, yet another typechecking issue.

--- src/pages/api/playlist.ts
+++ src/pages/api/playlist.ts
@@ -1,7 +1,7 @@
 import { validateRoute } from "src/lib/auth";
 import prisma from "src/lib/prisma";
 
-export default validateRoute(async (req, res, user) => {
+export default validateRoute(async (req: any, res: any, user: any) => {
   const playlists = await prisma.playlist.findMany({
     where: {
       userId: user.id,
